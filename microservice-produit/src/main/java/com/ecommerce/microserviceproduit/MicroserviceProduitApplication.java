6684cb1226ba07316bf05f2188e3dcdcfcafd608
Gaspi02 <duringer.gaspard@gmail.com>
2018-09-27 15:33:42 +0200

    Maj produit
    ajout des classes dans produit
    ajout de config server repo et config server

--- microservice-produit/src/main/java/com/ecommerce/microserviceproduit/MicroserviceProduitApplication.java
+++ microservice-produit/src/main/java/com/ecommerce/microserviceproduit/MicroserviceProduitApplication.java
@@ -3,6 +3,7 @@ package com.ecommerce.microserviceproduit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MicroserviceProduitApplication {
 
