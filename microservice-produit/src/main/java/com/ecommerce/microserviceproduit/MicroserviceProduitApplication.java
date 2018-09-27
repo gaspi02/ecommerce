c9ffed34bd024328a07866f1a91d0467c4148054
Gaspi02 <duringer.gaspard@gmail.com>
2018-09-27 14:55:49 +0200

    premier commit
    mise en place
    Eureka
    Zuul
    config server repo
    Microservice-produit

---
+++ microservice-produit/src/main/java/com/ecommerce/microserviceproduit/MicroserviceProduitApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.microserviceproduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProduitApplication.class, args);
+	}
+}
---
+++ microservice-produit/src/test/java/com/ecommerce/microserviceproduit/MicroserviceProduitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ecommerce.microserviceproduit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceProduitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
