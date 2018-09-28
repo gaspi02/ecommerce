485cda7308bf4b4d144d4c2b0e4c797bbe4e071c
Gaspi02 <duringer.gaspard@gmail.com>
2018-09-28 10:04:46 +0200

    remove microservice produit
    config server + eureka + zuul fonctionnel

--- microservice-produit/src/main/java/com/ecommerce/microserviceproduit/dao/ProductDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.microserviceproduit.dao;
-
-import com.ecommerce.microserviceproduit.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductDAO extends JpaRepository <Product, Integer>{
-}
